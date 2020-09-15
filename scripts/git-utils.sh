fbf9eaa693d76b24a8037eef9d94b3b038ce5482
qqliang <grapel2013@gmail.com>
2020-09-15 05:02:40 -0700

    Add git utils for correct author's info

---
+++ scripts/git-utils.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+function log_correct
+{
+git filter-branch --env-filter '
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+}
+
+function main
+{
+old_email= $1
+new_name= $2
+new_email= $3
+	gitlog_correct $old_email $new_name $new_email
+}
